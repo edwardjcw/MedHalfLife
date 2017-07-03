180bd75bac38529a9b2673e86ce465f39a92598c
Edward Conrad <edwardjcw@gmail.com>
2017-07-03 07:40:42 -0500

    minor

--- medhalflife.fs
+++ medhalflife.fs
@@ -7,16 +7,18 @@ type Dose = {origin:Time; amount:float; half_life:float}
 
 type Concentration() =
 
-    static let calculate (current_time : Time) (dose : Dose) =
+    static let calculate (current_time : Time) (dose : Dose option) =
         let t = function | Absolute d -> d | Relative d -> DateTime.MinValue
+        match dose with 
+        | None -> 0.0
+        | Some d ->
+            if t current_time < DateTime.MinValue ||
+                t d.origin > t current_time ||
+                d.half_life <= 0.0 ||
+                d.amount < 0.0 then 0.0
+            else d.amount * (0.5)**(((t current_time) - (t d.origin)).TotalHours/d.half_life)
 
-        if t current_time < DateTime.MinValue ||
-            t dose.origin > t current_time ||
-            dose.half_life <= 0.0 ||
-            dose.amount < 0.0 then 0.0
-        else dose.amount * (0.5)**(((t current_time) - (t dose.origin)).TotalHours/dose.half_life)
-
-    static member At (current_time : Time) (doses : Dose list) : float =
+    static member At (current_time : Time) (doses : Dose option list) : float =
         doses
         |> List.fold (fun level dose -> (calculate current_time dose) + level) 0.0
     
@@ -33,11 +35,14 @@ type Concentration() =
             | (false, _)  -> Absolute DateTime.MinValue
             | (true, d) -> Absolute d             
 
-let dose1 = {origin=Concentration.Time("8:31"); amount=15.0; half_life=2.0}
-let dose2 = {origin=Concentration.Time("11:13"); amount=10.0; half_life=2.0}
-let dose3 = {origin=Concentration.Time("14:00"); amount=10.0; half_life=2.0}
-let dose4 = {origin=Concentration.Time("15:25"); amount=5.0; half_life=2.0}
-let doses = [dose1; dose2; dose3; dose4]
+let dose1 = Some {origin=Concentration.Time("9:30"); amount=15.0; half_life=2.0}
+let dose2 = Some {origin=Concentration.Time("12:24"); amount=10.0; half_life=2.0}
+let dose3 = Some {origin=Concentration.Time("15:38"); amount=10.0; half_life=2.0}
+let dose4 = None // Some {origin=Concentration.Time("18:10"); amount=10.0; half_life=2.0}
+let dose5 = None // Some {origin=Concentration.Time("20:24"); amount=5.0; half_life=2.0}
+let doses : Dose option list = [dose1; dose2; dose3; dose4; dose5]
 
-System.Console.WriteLine("{0:0.##0}", Concentration.At (Concentration.Time("16:45")) doses)
-System.Console.ReadKey() |> ignore
+let current = Concentration.At (Absolute DateTime.Now) doses
+
+System.Console.WriteLine("{0:0.##0}", Concentration.At (Absolute DateTime.Now) doses)
+//System.Console.ReadKey() |> ignore
